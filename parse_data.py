19b52a90042ad7955a09e05fa20f68b8290de220
Gabe Krishnadasan <gkrishnadasan@sandiego.edu>
2023-08-29 11:07:48 -0700

    Finished parsing data

--- parse_data.py
+++ parse_data.py
@@ -11,7 +11,7 @@ def parse_html(box_score):
     with open(box_score) as f:
         html = f.read()
 
-    soup =  BeautifulSoup(html)
+    soup =  BeautifulSoup(html, features="lxml")
     [s.decompose() for s in soup.select("tr.over_header")]
     [s.decompose() for s in soup.select("tr.thead")]
     return soup
@@ -32,3 +32,62 @@ def read_stats(soup, team, stat):
     df = df.apply(pd.to_numeric, errors="coerce")
     return df
 
+def read_season_info(soup):
+    nav = soup.select("#bottom_nav_container")[0]
+    hrefs = [a["href"] for a in nav.find_all("a")]
+    season = os.path.basename(hrefs[1]).split("_")[0]
+    return season
+
+base_cols = None
+games = []
+
+for box_score in box_scores:
+    soup = parse_html(box_score)
+    line_score = read_line_score(soup)
+    teams = list(line_score["team"])
+
+    summaries = []
+    for team in teams:
+        basic = read_stats(soup, team, "basic")
+        adv = read_stats(soup, team, "advanced")
+        
+        total = pd.concat([basic.iloc[-1,:], adv.iloc[-1,:]])
+        total.index = total.index.str.lower()
+
+        maxes = pd.concat([basic.iloc[:-1,:].max(), adv.iloc[:-1,:].max()])
+        maxes.index = maxes.index.str.lower() + "_max"
+
+        summary = pd.concat([total, maxes])
+
+        if not base_cols:
+            base_cols = list(summary.index.drop_duplicates(keep="first"))
+            for b in base_cols:
+                if "bpm" in b:
+                    base_cols.remove(b)
+
+        sumamry = summary[base_cols]
+
+        summaries.append(sumamry)
+
+    summary = pd.concat(summaries, axis=1).T
+
+    game = pd.concat([summary, line_score], axis=1)
+
+    game["home"] = [0, 1]
+    game_opp = game.iloc[::-1].reset_index()
+    game_opp.columns += "_opp"
+
+    full_game = pd.concat([game, game_opp], axis=1)
+
+    full_game["season"] = read_season_info(soup)
+    full_game["date"] = os.path.basename(box_score)[:8]
+    full_game["date"] = pd.to_datetime(full_game["date"], format="%Y%m%d")
+
+    full_game["won"] = full_game["total"] > full_game["total_opp"]
+    games.append(full_game)
+
+    if len(games) % 100 == 0:
+        print(f"{len(games)} / {len(box_scores)}")
+
+games_df = pd.concat(games, ignore_index=True)
+games_df.to_csv("nba_games.csv")
